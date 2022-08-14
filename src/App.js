c285eb8cdf88228e46e52d0c33dff822c9f6b90d
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-15 02:17:43 +0300

    Add todo list component using hooks

---
+++ src/App.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { useState, useEffect } from 'react';
+import './index.css'
+import Header from './components/header';
+import AddTask from './components/form';
+import List from './components/list';
+
+const App = () => {
+  const allTask = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(allTask);
+  const [editTodo, setEditTodo] = useState("null");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  return (
+    <div className="container">
+    <div className="wrapper">
+    <div className="header">
+    <Header />
+    </div>
+    <div>
+     <AddTask input={input}
+      setInput={setInput}
+      todos={todos}
+      setTodos={setTodos}
+      editTodo={editTodo}
+      setEditTodo={setEditTodo}
+      />
+    </div>
+    <div>
+      <List todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+    </div>
+    </div>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header"> 
+             <h2>Todo List</h2>
+            </div>
+        )
+    }
+}
+
+export default Header;
