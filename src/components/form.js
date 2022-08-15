a57d7c5c295e6a07d55cea2ed31eb7631d8a9c9e
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-15 19:53:23 +0300

    Fix linter errors

--- src/components/form.js
+++ src/components/form.js
@@ -1,54 +1,58 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const AddTask = ({
-  input, setInput, todos, setTodos, editTodo, setEditTodo,
+    // eslint-disable-next-line react/prop-types
+    input, setInput, todos, setTodos, editTodo, setEditTodo,
 }) => {
-  const updateTodo = (description, id, completed) => {
-    const newTask = todos.map((task) => 
-      task.id === id ? { description, id, completed } : task
+    const updateTodo = (description, id, completed) => {
+        // eslint-disable-next-line react/prop-types
+        const newTask = todos.map((task) => 
+            task.id === id ? { description, id, completed } : task
+        );
+        setTodos(newTask);
+        setEditTodo('');
+    };
+
+    useEffect(() => {
+        if(editTodo){
+            // eslint-disable-next-line react/prop-types
+            setInput(editTodo.description);
+        }
+        else {
+            setInput('');
+        }
+    }, [setInput, editTodo]);
+
+    const handlenewTask = (e) => {
+        e.preventDefault();
+        setInput(e.target.value);
+    };
+
+    const addTask = (e) => {
+        e.preventDefault();
+        if(!editTodo) {
+            setTodos([...todos, { id: uuidv4(), description: input, completed: false }]);
+            setInput('');
+        }
+        else {
+            // eslint-disable-next-line react/prop-types
+            updateTodo(input, editTodo.id, editTodo.completed);
+        }
+
+    };
+    return (
+        <form className="form" onSubmit={addTask}>
+            <input type="text" className="newTodo"
+                placeholder="Add to your list..." required
+                value={input}
+                onChange={handlenewTask}
+            />
+            <button className="add" type="submit">
+                {editTodo ? 'Edit' : 'Add'}
+            </button>
+        </form>
     );
-    setTodos(newTask);
-    setEditTodo('');
-   };
-
-  useEffect(() => {
-    if(editTodo){
-      setInput(editTodo.description);
-    }
-    else {
-      setInput('');
-    }
-  }, [setInput, editTodo]);
-
-  const handlenewTask = (e) => {
-    e.preventDefault();
-    setInput(e.target.value);
-};
-
-  const addTask = (e) => {
-    e.preventDefault();
-    if(!editTodo) {
-      setTodos([...todos, { id: uuidv4(), description: input, completed: false }]);
-      setInput('');
-    }
-    else {
-      updateTodo(input, editTodo.id, editTodo.completed);
-    }
-
-};
-  return (
-    <form className="form" onSubmit={addTask}>
-      <input type="text" className="newTodo"
-       placeholder="Add to your list..." required
-       value={input}
-       onChange={handlenewTask}
-        />
-      <button className="add" type="submit">
-                {editTodo ? "Edit" : "Add"}
-      </button>
-    </form>
-  );
 };
 
 export default AddTask;
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,14 @@
 /* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
-    return (
-        <div className="header">
-          <h2>Todo List</h2>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="header">
+                <h2>Todo List</h2>
+            </div>
+        );
+    }
 }
 
 export default Header;
